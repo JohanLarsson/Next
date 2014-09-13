4994c848a0fb6a0aba059ce589ce72b4f5d146d8
Johan Larsson <johan_gavle1@hotmail.com>
2014-09-13 16:06:44 +0200

    Added logging of unknown & error messages for feed

---
+++ NextView/FeedEvent.cs
@@ -0,0 +1,27 @@
+﻿namespace NextView
+{
+    using System;
+
+    public class FeedEvent
+    {
+        private FeedEvent(string readOrWrote, string message)
+        {
+            this.TimeStamp = DateTime.UtcNow;
+            this.Action = readOrWrote;
+            this.Message = message;
+        }
+        public static FeedEvent Wrote(string message)
+        {
+            return new FeedEvent("Wrote", message);
+        }
+        public static FeedEvent Read(string message)
+        {
+            return new FeedEvent("Read", message);
+        }
+        public DateTime TimeStamp { get; private set; }
+
+        public string Action { get; private set; }
+
+        public string Message { get; set; }
+    }
+}
--- NextView/FeedVm.cs
+++ NextView/FeedVm.cs
@@ -8,17 +8,17 @@ using NextView.Annotations;
 
 namespace NextView
 {
-    using System;
-
     public class FeedVm : INotifyPropertyChanged
     {
         private readonly ObservableCollection<FeedEvent> _messages = new ObservableCollection<FeedEvent>();
-
+        private static readonly log4net.ILog Log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         public FeedVm(NextFeed feed)
         {
             Feed = feed;
             Feed.ReceivedSomething += (o, s) => Application.Current.Dispatcher.Invoke(() => Messages.Insert(0, FeedEvent.Read(s)));
             Feed.WroteSomething += (o, s) => Application.Current.Dispatcher.Invoke(() => Messages.Insert(0, FeedEvent.Wrote(s)));
+            Feed.ReceivedUnknownMessage += (sender, s) => Log.DebugFormat("feed:{0} unknown: {1}", sender.GetType().Name, s);
+            Feed.ReceivedError += (sender, s) => Log.DebugFormat("feed:{0} error: {1}", sender.GetType().Name, s);
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -37,28 +37,5 @@ namespace NextView
             PropertyChangedEventHandler handler = PropertyChanged;
             if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
         }
-    }
-
-    public class FeedEvent
-    {
-        private FeedEvent(string readOrWrote, string message)
-        {
-            TimeStamp = DateTime.UtcNow;
-            Action = readOrWrote;
-            Message = message;
-        }
-        public static FeedEvent Wrote(string message)
-        {
-            return new FeedEvent("Wrote", message);
-        }
-        public static FeedEvent Read(string message)
-        {
-            return new FeedEvent("Read", message);
-        }
-        public DateTime TimeStamp { get; private set; }
-
-        public string Action { get; private set; }
-
-        public string Message { get; set; }
     }
 }
